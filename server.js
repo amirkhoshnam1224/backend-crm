58961cbf1cace0c383134415565a280a0a319ab2
amirkhoshnam1224 <amir.khoshnam1224@gmail.com>
2025-01-09 15:29:19 +0330

    Updated MongoDB connection to Atlas

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes'); // مسیر صحیح
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 
 // استفاده از middleware ها
 app.use(cors());
 app.use(express.json());
 
 // اتصال به MongoDB
-mongoose.connect('mongodb://localhost/crm', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('MongoDB connection error:', err));
+mongoose.connect('mongodb+srv://amirkhoshnam1224:kR1JOV1zEqpPM5zQ@cluster0.06mn0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.log('MongoDB Atlas connection error:', err));
 
 // استفاده از مسیر کاربران
 app.use('/api/users', userRoutes);
