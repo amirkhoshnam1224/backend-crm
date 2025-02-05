a1f2b649f4d174dafffd36d2aefc9a7f0f81c67c
amirkhoshnam1224 <amir.khoshnam1224@gmail.com>
2025-02-05 16:26:03 +0330

    Added user-related updates

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getUsers, addUser, deleteUser } = require("../controllers/userController");
+const { getUsers, addUser, deleteUser,updateUser } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/", addUser);
 router.delete("/:id", deleteUser);
+router.put("/:id",updateUser)
 
 module.exports = router;
